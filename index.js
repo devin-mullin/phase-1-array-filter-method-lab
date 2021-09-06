125f2298f9502805e0f4699936482897949ef024
Devin Mullin <devin.mullin90@gmail.com>
2021-09-06 15:41:48 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(drivers, string){
+   return drivers.filter(function(driver){
+       return driver.toLowerCase() === string.toLowerCase()
+   })
+}
+
+function fuzzyMatch(drivers, string){
+    let firstLet = drivers.filter(firstLetters => firstLetters.substring(0, 2) === string)
+    return firstLet
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(driver){
+        return driver.name === string
+    })
+}
+  
